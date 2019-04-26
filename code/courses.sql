48d4b3539717ba18782df9a32c0088c5926166ed
Scott <scorobers@gmail.com>
2019-04-26 08:43:35 -0700

    add get/post with SQL'

--- code/courses.sql
+++ code/courses.sql
@@ -6,5 +6,8 @@ shortDesc VARCHAR(255) NOT NULL,
 longDesc VARCHAR(255) NOT NULL,
 prereqs VARCHAR(255) NOT NULL);
 
-INSERT INTO Courses
-VALUES('TCSS343', 'Algorithms', 'A very hard class', 'TCSS342');
+
+INSERT INTO Courses VALUES('TCSS343', 'Algorithms', 'A very hard class', 'TCSS342');
+INSERT INTO Courses VALUES('TCSS450', 'Algorithms', 'A very hard class', 'TCSS342');
+INSERT INTO Courses VALUES('TCSS342', 'Algorithms', 'A very hard class', 'TCSS342');
+INSERT INTO Courses VALUES('TCSS360', 'Algorithms', 'A very hard class', 'TCSS342');
