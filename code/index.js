48b7f9dce832f69185ade6e7d18ad924f44114b7
Scott <scorobers@gmail.com>
2019-04-27 16:42:12 -0700

    add get/post with SQL again?

--- code/index.js
+++ code/index.js
@@ -95,55 +95,6 @@ app.get("/courses", (req, res) => {
     });
 });
 
-app.post("/addcourse", (req, res) => {
-    // Parameters for the courses
-    let id = req.body['id'];
-    let shortdesc = req.body['shortdesc'];
-    let longdesc = req.body['longdesc'];
-    let prereqs = req.body['prereqs'];
-
-    if (id && shortdesc && longdesc && prereqs) {
-        db.none("INSERT INTO courses VALUES ($1, $2, $3, $4)", [id, shortdesc, longdesc, prereqs])
-            .then(() => {
-                //We successfully added the course, let the user know
-                res.send({
-                    success: true
-                });
-            }).catch((err) => {
-            //log the error
-            console.log(err);
-            res.send({
-                success: false,
-                error: err
-            });
-        });
-    } else {
-        res.send({
-            success: false,
-            input: req.body,
-            error: "Missing required information"
-        });
-    }
-});
-
-app.get("/courses", (req, res) => {
-
-    db.manyOrNone('SELECT * FROM courses')
-    //If successful, run function passed into .then()
-        .then((data) => {
-            res.send({
-                success: true,
-                names: data
-            });
-        }).catch((error) => {
-        console.log(error);
-        res.send({
-            success: false,
-            error: error
-        })
-    });
-});
-
 /* 
 * Heroku will assign a port you can use via the 'PORT' environment variable
 * To accesss an environment variable, use process.env.<ENV>
