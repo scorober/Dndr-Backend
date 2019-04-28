e2dc30343d55f2181338ef5fb5ff3d1f64926a87
Scott <scorobers@gmail.com>
2019-04-27 17:13:07 -0700

    moved db to endpoints

--- code/index.js
+++ code/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use('/hello', require('./routes/hello.js'));
 app.use('/params', require('./routes/params.js'));
 app.use('/wait', require('./routes/wait.js'));
+app.use('/', require('./routes/coursessql.js'));
 
 const bodyParser = require("body-parser");
 //This allows parsing of the body of POST requests, that are encoded in JSON
@@ -44,56 +45,6 @@ app.get("/", (req, res) => {
     res.end(); //end the response
 });
 
-
-
-app.post('/addcourse', (req, res) => {
-    // Parameters for the courses.
-    let id = req.body['id'];
-    let shortdesc = req.body['shortdesc'];
-    let longdesc = req.body['longdesc'];
-    let prereqs = req.body['prereqs'];
-
-    if (id && shortdesc && longdesc && prereqs) {
-        db.none("INSERT INTO courses VALUES ($1, $2, $3, $4)", [id, shortdesc, longdesc, prereqs])
-            .then(() => {
-                //We successfully added the course, let the user know
-                res.send({
-                    success: true
-                });
-            }).catch((err) => {
-                //log the error
-                console.log(err);
-                res.send({
-                    success: false,
-                    error: err
-            });
-        });
-    } else {array
-        res.send({
-            success: false,
-            input: req.body,
-            error: "Missing required information"
-        });
-    }
-});
-
-app.get('/courses', (req, res) => {
-
-    db.manyOrNone('SELECT * FROM courses')
-    //If successful, run function passed into .then()
-        .then((data) => {
-            res.send({
-                success: true,
-                names: data
-            });
-        }).catch((error) => {
-            console.log(error);
-            res.send({
-                success: false,
-                error: error
-        })
-    });
-});
 
 /* 
 * Heroku will assign a port you can use via the 'PORT' environment variable
